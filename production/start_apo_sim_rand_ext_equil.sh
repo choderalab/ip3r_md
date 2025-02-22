e889436b51c9442ab957de9db014bb3a4a41b8a5
Belay <viktorbelay@gmail.com>
2025-02-22 17:50:42 -0500

    fix shell scripts

--- production/start_apo_sim_rand_ext_equil.sh
+++ production/start_apo_sim_rand_ext_equil.sh
@@ -6,9 +6,9 @@ mkdir sim_rand_ext_equil_$1_$2
 
 # Copy py script for running sim
 
-cp ../../scripts/sim_ligand_500ns_rand.py sim_rand_ext_equil_$1_$2/.
+cp ../../scripts/sim_apo_500ns_rand.py sim_rand_ext_equil_$1_$2/.
 cp ../../scripts/sim.sh sim_rand_ext_equil_$1_$2/.
-cp ../../scripts/sim_params_ext_equil.yaml sim_rand_ext_equil_$1_$2/sim_params.yaml
+cp ../../scripts/sim_params_ext_equil_apo.yaml sim_rand_ext_equil_$1_$2/sim_params.yaml
 # Start simulation
 cd sim_rand_ext_equil_$1_$2
 bsub < sim.sh
